66eb58403aa759e9d37616a36f62b2d346bdaf93
Franklin ngangu simbi <143759747+EFTECH05@users.noreply.github.com>
2026-05-15 06:35:18 +0200

    Initial GLMS commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        // REPLACED FEATURES WITH CONTACT US
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult Quote()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
+        }
+    }
+}
